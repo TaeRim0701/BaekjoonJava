abbe092a27e50b551fdf4171727a6c69eed4d646
taerim0701 <xofla71@naver.com>
2025-07-23 16:11:19 +0900

    [Success] Second try_추후 공부 필요

--- Baekjoon2749.java
+++ Baekjoon2749.java
@@ -1,21 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Baekjoon2749 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 백준에서 많은 양의 입력을 처리할 때 Scanner보다 BufferedReader가 더 효율적
+        Long N = 0L;
 
-        int fib = fibonacci(n);
+        int M = 1_000_000;
+        int P = 1_500_000; //피사노 주기 : M = 10^k 일 때, k > 2 라면, 주기는 항상 15 × 10^(k-1)
 
-        System.out.println(fib % 1000000);
-    }
-
-    public static int fibonacci(int n) {
-        if (n == 0) return 0;
-        else if (n == 1) return 1;
-        else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
+        try {
+            N = Long.parseLong(br.readLine());
+        } catch (IOException e) { // 입출력 오류
+            System.out.println("IOException");
+            System.exit(1);
         }
-    }
 
+        int[] fibo = new int[(int)(N % P) + 1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for (int i = 2; i < fibo.length; i++) {
+            fibo[i] = (fibo[i - 2] + fibo[i - 1]) % M;
+        }
+
+        System.out.println(fibo[(int)(N % P)]);
+    }
 }
